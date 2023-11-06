b4d177873228095f9bb31e47d61ba7486d1cad1f
ljenchik <pelagenkolena@gmail.com>
2023-11-06 12:41:05 +0000

    Added app.get to display all musicians in localhost:3000

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
-
-
-
-
-
-
+app.get("/musicians", async (req, res) => {
+    const response = await Musician.findAll();
+    res.json(response);
+});
 
 module.exports = app;
