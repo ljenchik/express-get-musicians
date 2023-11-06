f6b5cfbbc453ccb270619901c41085c9b8ef4073
ljenchik <pelagenkolena@gmail.com>
2023-11-06 13:54:28 +0000

    Added tests for band endpoint

--- index.test.js
+++ index.test.js
@@ -1,22 +1,91 @@
-// install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const request = require("supertest");
+const app = require("./src/app");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const seedMusician = require("./seedData");
+describe("Testing musicians endpoints", () => {
+    test("Testing musicians endpoint", async () => {
+        const response = await request(app).get("/musicians");
+        //console.log(response.body);
+        expect(response.body.length).toBe(3);
+        expect(response.statusCode).toBe(200);
+        const data = JSON.parse(response.text);
+        //console.log(data);
+        expect(data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    name: "Mick Jagger",
+                    instrument: "Voice",
+                }),
+            ])
+        );
+        expect(data).toEqual(
+            expect.arrayContaining([expect.objectContaining({ name: "Drake" })])
+        );
+        expect(data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ name: "Jimi Hendrix" }),
+            ])
+        );
+    });
 
+    test("Testing musicians/1 endpoint", async () => {
+        const response = await request(app).get("/musicians/1");
+        expect(response.statusCode).toBe(200);
+        const data = JSON.parse(response.text);
+        expect(data).toEqual(
+            expect.objectContaining({
+                name: "Mick Jagger",
+                instrument: "Voice",
+            })
+        );
+    });
+    test("Testing musicians/2 endpoint", async () => {
+        const response = await request(app).get("/musicians/2");
+        expect(response.statusCode).toBe(200);
+        const data = JSON.parse(response.text);
+        expect(data).toEqual(
+            expect.objectContaining({
+                name: "Drake",
+                instrument: "Voice",
+            })
+        );
+    });
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+    test("Testing musicians/3 endpoint", async () => {
+        const response = await request(app).get("/musicians/3");
+        expect(response.statusCode).toBe(200);
+        const data = JSON.parse(response.text);
+        expect(data).toEqual(
+            expect.objectContaining({
+                name: "Jimi Hendrix",
+                instrument: "Guitar",
+            })
+        );
+    });
+});
 
-
-
-
-    
-})
+describe("Testing bands endpoints", () => {
+    test("Testing all bands endpoint", async () => {
+        const response = await request(app).get("/bands");
+        expect(response.body.length).toBe(3);
+        expect(response.statusCode).toBe(200);
+        const data = JSON.parse(response.text);
+        //console.log(data);
+        expect(data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    name: "The Beatles",
+                }),
+            ])
+        );
+        expect(data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ name: "Black Pink" }),
+            ])
+        );
+        expect(data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ name: "Coldplay" }),
+            ])
+        );
+    });
+});
--- src/app.js
+++ src/app.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index");
+const { Musician, Band } = require("../models/index");
 const { db } = require("../db/connection");
 
 const port = 3000;
 
 app.get("/musicians", async (req, res) => {
-    const response = await Musician.findAll();
-    res.json(response);
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+});
+
+app.get("/musicians/1", async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians[0]);
+});
+
+app.get("/musicians/2", async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians[1]);
+});
+
+app.get("/musicians/3", async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians[2]);
+});
+
+app.get("/bands", async (req, res) => {
+    const bands = await Band.findAll();
+    res.json(bands);
 });
 
 module.exports = app;
